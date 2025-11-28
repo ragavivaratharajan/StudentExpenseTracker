d9b369876ca736cc9ecb4243296e309ceeb05646
ragavivaratharajan <ragavivaratharajanahalya@gmail.com>
2025-11-28 19:54:24 +0000

    BudgetExceededException to custom immutable type

--- src/studentexpensetracker/exceptions/BudgetExceededException.java
+++ src/studentexpensetracker/exceptions/BudgetExceededException.java
@@ -6,11 +6,17 @@ package studentexpensetracker.exceptions;
 /**
  * Custom checked exception which is thrown when expenses exceed the user's budget.
  */
-public class BudgetExceededException extends Exception {
+public final class BudgetExceededException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
+	private final String message;
 	
 	public BudgetExceededException(String message) {
         super(message);
+        this.message = message;
     }
+	
+	public String getMessage() {
+		return message;
+	}
 }
